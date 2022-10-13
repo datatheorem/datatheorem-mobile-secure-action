f3cb67a7417281c404939c7c3d40f7cceef53db2
Victor Wang <vw@datatheorem.io>
2022-10-13 14:51:58 +0200

    Support file path glob

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const glob = require("glob");
 const FormData = require("form-data");
 const fs = require("fs");
 function run() {
@@ -19,58 +20,76 @@ function run() {
             // Get inputs
             const dt_upload_api_key = core.getInput("DT_UPLOAD_API_KEY");
             const input_binary_path = core.getInput("UPLOAD_BINARY_PATH");
+            // Mask the API key
             core.setSecret(dt_upload_api_key);
-            if (!fs.existsSync(input_binary_path)) {
-                throw new Error("Input file does not exist at " + input_binary_path);
+            // Check that the inputs are set
+            if (!dt_upload_api_key) {
+                throw new Error("DT_UPLOAD_API_KEY must be set!");
             }
-            // retry upload 3 times
-            for (let loop_idx = 0; loop_idx < 3; loop_idx++) {
-                // Send the auth request to get the upload URL
-                const auth_response = yield fetch("https://api.securetheorem.com/uploadapi/v1/upload_init", {
-                    method: 'POST',
-                    headers: {
-                        Authorization: "APIKey " + dt_upload_api_key,
-                        Accept: "application/json",
-                        "Content-Type": "application/json",
-                    },
-                });
-                let auth_json;
-                try {
-                    auth_json = yield auth_response.json();
+            if (!input_binary_path) {
+                throw new Error("UPLOAD_BINARY_PATH must be set!");
+            }
+            const files = glob.sync(input_binary_path);
+            if (!files.length) {
+                throw new Error("Did not find any files that match path:" + input_binary_path);
+            }
+            console.log("Found files matching path: " + input_binary_path);
+            console.log(files);
+            // Upload all the files that matched the file path
+            for (const file_path of files) {
+                if (!fs.existsSync(file_path)) {
+                    throw new Error("Could not find file:" + file_path);
                 }
-                catch (err) {
-                    core.setFailed(err);
-                }
-                if (auth_response.status !== 200) {
-                    // handles auth failure
-                    core.setFailed(auth_json);
-                    break;
-                }
-                const form = new FormData();
-                form.append('file', fs.createReadStream(input_binary_path));
-                // Send the scan request with file
-                const response = yield fetch(auth_json.upload_url, {
-                    method: 'POST',
-                    body: form,
-                });
-                let jsonformat;
-                try {
-                    jsonformat = yield response.json();
-                }
-                catch (err) {
-                    core.setFailed(err);
-                }
-                // Check the response
-                console.log(jsonformat);
-                if (response.status === 200) {
-                    core.setOutput('response', jsonformat);
-                    ;
-                    break;
-                }
-                else if (loop_idx == 2) {
-                    core.setFailed(jsonformat);
+                // retry upload 3 times
+                for (let loop_idx = 0; loop_idx < 3; loop_idx++) {
+                    // Send the auth request to get the upload URL
+                    const auth_response = yield fetch("https://api.securetheorem.com/uploadapi/v1/upload_init", {
+                        method: "POST",
+                        headers: {
+                            Authorization: "APIKey " + dt_upload_api_key,
+                            Accept: "application/json",
+                            "Content-Type": "application/json",
+                        },
+                    });
+                    let auth_json;
+                    try {
+                        auth_json = yield auth_response.json();
+                    }
+                    catch (err) {
+                        core.setFailed(err);
+                    }
+                    if (auth_response.status !== 200) {
+                        // handles auth failure
+                        core.setFailed(auth_json);
+                        break;
+                    }
+                    const form = new FormData();
+                    form.append("file", fs.createReadStream(file_path));
+                    // Send the scan request with file
+                    console.log("Starting upload of:" + file_path);
+                    const response = yield fetch(auth_json.upload_url, {
+                        method: "POST",
+                        body: form,
+                    });
+                    console.log("Finished upload of:" + file_path);
+                    let jsonformat;
+                    try {
+                        jsonformat = yield response.json();
+                    }
+                    catch (err) {
+                        core.setFailed(err);
+                    }
+                    // Check the response
+                    if (response.status === 200) {
+                        console.log(jsonformat);
+                        break;
+                    }
+                    else if (loop_idx == 2) {
+                        core.setFailed(jsonformat);
+                    }
                 }
             }
+            core.setOutput("response", "All done !");
         }
         catch (err) {
             core.setFailed(err.message);
