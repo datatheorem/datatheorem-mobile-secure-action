b788828a76be7736f417c2e88d48f96778fdaa51
Igor Matlin <igor.matlin@gmail.com>
2023-04-03 17:14:08 -0500

    SASB-3767 Added support for optional parameters

--- main.js
+++ main.js
@@ -14,27 +14,41 @@ const fetch = require("node-fetch");
 const glob = require("glob");
 const FormData = require("form-data");
 const fs = require("fs");
+
+// Global constants
+const maxUploadFiles = 3; // no more than 3 files can be uploaded at a time
+
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             // Get inputs
+            // Required
             const dt_upload_api_key = core.getInput("DT_UPLOAD_API_KEY");
             const input_binary_path = core.getInput("UPLOAD_BINARY_PATH");
-            // Mask the API key
+            // Optional
+            const username = core.getInput("USERNAME");
+            const password = core.getInput("PASSWORD");
+            const comments = core.getInput("COMMENTS");
+            const release_id = core.getInput("RELEASE_ID");
+            const platform_variant = core.getInput("PLATFORM_VARIANT");
+            const external_id = core.getInput("EXTERNAL_ID");
+            // Mask sensitive fields
             core.setSecret(dt_upload_api_key);
-            // Check that the inputs are set
+            core.setSecret(password);
+            // Check that the required inputs are set
             if (!dt_upload_api_key) {
                 throw new Error("DT_UPLOAD_API_KEY must be set!");
             }
             if (!input_binary_path) {
                 throw new Error("UPLOAD_BINARY_PATH must be set!");
             }
+            // get the list of files to upload
             const files = glob.sync(input_binary_path);
             if (!files.length) {
                 throw new Error("Did not find any files that match path:" + input_binary_path);
             }
-            if (files.length > 3) {
-                throw new Error("Too many files match the provided glob pattern, please write a more restrictive pattern");
+            if (files.length > maxUploadFiles) {
+                throw new Error("Too many files (" + files.length + ") match the provided glob pattern; please write a more restrictive pattern to match no more than " + maxUploadFiles + " files.");
             }
             // Upload all the files that matched the file path
             let output = [];
@@ -67,13 +81,38 @@ function run() {
                     }
                     const form = new FormData();
                     form.append("file", fs.createReadStream(file_path));
-                    // Send the scan request with file
-                    console.log("Starting upload of:" + file_path);
+                    // only append optional fields if explicitly set 
+                    if(username) {
+                        form.append("username", username);
+                        console.log("DAST username set to: " + username);  
+                    }
+                    if(password) {
+                        form.append("password", password);
+                        console.log("DAST password is set");
+                    }
+                    if(comments) {
+                        form.append("comments", comments);
+                        console.log("Comments are set to: " + comments);
+                    }
+                    if(release_id) {
+                        form.append("release_Id", release_id);
+                        console.log("Release ID is set to: " + release_id);
+                    }
+                    if(platform_variant) {
+                        form.append("platform_variant", platform_variant);
+                        console.log("Platform variant is set to: " + platform_variant);
+                    }
+                    if(external_id) {
+                        form.append("external_id", external_id);
+                        console.log("External ID is set to: " + external_id);
+                    }
+                    // Send the scan request with file and optional fields
+                    console.log(`Starting upload of ${file_path}`);
                     const response = yield fetch(auth_json.upload_url, {
                         method: "POST",
                         body: form,
                     });
-                    console.log("Finished upload of:" + file_path);
+                    console.log(`Finished upload of ${file_path}`);
                     let jsonformat;
                     try {
                         jsonformat = yield response.json();
