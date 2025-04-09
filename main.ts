2caec5506b006a6846cd9ee82d2e4bf9c86f4d1e
Alexis Pierru <ap@datatheorem.io>
2025-04-09 02:02:57 +0200

    Start refactoring the script

--- main.ts
+++ main.ts
@@ -8,17 +8,128 @@ import fs = require("fs");
 const maxUploadFiles: number = 3; // no more than 3 files can be uploaded at a time
 const maxRetries: number = 3; // max number of uploads to retry
 
-async function upload_step_init(dt_upload_api_key: string): Promise<Response> {
-  return await fetch(
+class UploadInitError extends Error {
+  response: Response;
+
+  constructor(response: Response, ...rest) {
+    super(...rest);
+    this.name = "UploadInitError";
+    this.response = response;
+  }
+}
+
+class UploadLastError extends Error {
+  status: Number;
+
+  constructor(status: Number, ...rest) {
+    super(...rest);
+    this.status = status;
+  }
+}
+
+async function uploadStepInit(
+  uploadApiKey: string,
+): Promise<string> {
+  const response = await fetch(
     "https://api.securetheorem.com/uploadapi/v1/upload_init",
     {
       "headers": {
         "Accept": "application/json",
-        "Authorization": "APIKey " + dt_upload_api_key,
+        "Authorization": `APIKey ${uploadApiKey}`,
         "Content-Type": "application/json",
       },
       "method": "POST",
     },
+  );
+
+  if (response.status !== 200) {
+    throw new UploadInitError(response);
+  }
+
+  const jsonData = await response.json().catch(
+    err => {
+      throw new UploadInitError(response, {cause: err});
+    }
+  );
+
+  if (typeof jsonData !== "object") {
+    throw new UploadInitError(response);
+  }
+
+  const {"upload_url": uploadUrl} = jsonData;
+
+  if (typeof uploadUrl !== "string") {
+    throw new UploadInitError(response)
+  }
+
+  return uploadUrl;
+}
+
+async function uploadStepLast(
+  uploadUrl: string,
+  options: {
+    file: fs.ReadStream,
+    sourcemap: fs.ReadStream | undefined,
+    credentials: {
+      comments: string | undefined,
+      password: string,
+      username: string,
+    } | undefined,
+    externalId: string | undefined,
+    platformVariant: string | undefined,
+    releaseId: string | undefined,
+  },
+): Promise<Response> {
+  const form = new FormData();
+
+  form.append("file", options.file);
+
+  if (options.sourcemap) {
+    form.append("sourcemap", options.sourcemap);
+  }
+
+  // only append optional fields if explicitly set
+  if (options.credentials) {
+    form.append("username", username);
+    console.log(`DAST username set to: ${username}`);
+  }
+  if (password) {
+    form.append("password", password);
+    console.log("DAST password is set to: (hidden)");
+  }
+  if (comments) {
+    form.append("comments", comments);
+    console.log(`Comments are set to: ${comments}`);
+  }
+  if (release_id) {
+    form.append("release_Id", release_id);
+    console.log(`Release ID is set to: ${release_id}`);
+  }
+  if (platform_variant) {
+    form.append("platform_variant", platform_variant);
+    console.log("Platform variant is set to: " + platform_variant);
+  }
+  if (external_id) {
+    form.append("external_id", external_id);
+    console.log("External ID is set to: " + external_id);
+  }
+
+  const response = await fetch(
+    uploadUrl,
+    {
+      "body": form,
+      "method": "POST",
+    },
+  );
+
+  if (response.status !== 200) {
+    throw new UploadLastError(response);
+  }
+
+  return await response.json().catch(
+    err => {
+      throw new UploadLastError(response, {cause: err});
+    }
   );
 }
 
@@ -29,13 +140,15 @@ async function run() {
   const input_binary_path = core.getInput("UPLOAD_BINARY_PATH");
 
   // Optional
-  const sourcemap_file_path = core.getInput("SOURCEMAP_FILE_PATH");
-  const username = core.getInput("USERNAME");
-  const password = core.getInput("PASSWORD");
+  const sourcemapFilePath = core.getInput("SOURCEMAP_FILE_PATH");
+  // - DAST credentials
   const comments = core.getInput("COMMENTS");
-  const release_id = core.getInput("RELEASE_ID");
-  const platform_variant = core.getInput("PLATFORM_VARIANT");
+  const password = core.getInput("PASSWORD");
+  const username = core.getInput("USERNAME");
+  // - Release information
   const external_id = core.getInput("EXTERNAL_ID");
+  const platform_variant = core.getInput("PLATFORM_VARIANT");
+  const release_id = core.getInput("RELEASE_ID");
 
   // Mask the sensitive fields
   core.setSecret(dt_upload_api_key);
@@ -55,110 +168,35 @@ async function run() {
   }
   if (files.length > maxUploadFiles) {
     throw new Error(
-      "Too many files (" + files.length + ") match the provided glob pattern; please write a more restrictive pattern to match no more than " + maxUploadFiles + " files."
+      `Too many files (${files.length}) match the provided glob pattern; please write a more restrictive pattern to match no more than ${maxUploadFiles} files.`
     );
   }
 
-  console.log("Found " + files.length + " files to upload.");
+  console.log(`Found ${files.length} files to upload.`);
 
   // Upload all the files that matched the file path
-  let file_idx: number = 1;
   let output: Array<any> = []
-  for (const file_path of files) {
-    if (!fs.existsSync(file_path)) {
-      throw new Error("Could not access file:" + file_path);
-    }
 
-    console.log("Processing file " + file_path + " (" + file_idx + " of " + files.length + ").");
-
-    const form = new FormData();
-    form.append("file", fs.createReadStream(file_path));
-
-    if (sourcemap_file_path) {
-      try {
-        form.append("sourcemap", fs.createReadStream(sourcemap_file_path));
-      } catch (err) {
-        core.setFailed(err);
-        return;
-      }
-    }
-
-    // only append optional fields if explicitly set
-    if (username) {
-      form.append("username", username);
-      console.log("DAST username set to: " + username);
-    }
-    if (password) {
-      form.append("password", password);
-      console.log("DAST password is set to: (hidden)");
-    }
-    if (comments) {
-      form.append("comments", comments);
-      console.log("Comments are set to: " + comments);
-    }
-    if (release_id) {
-      form.append("release_Id", release_id);
-      console.log("Release ID is set to: " + release_id);
-    }
-    if (platform_variant) {
-      form.append("platform_variant", platform_variant);
-      console.log("Platform variant is set to: " + platform_variant);
-    }
-    if (external_id) {
-      form.append("external_id", external_id);
-      console.log("External ID is set to: " + external_id);
-    }
+  for (const [file_idx, file_path] of files.entries()) {
+    console.log(`Processing file ${file_path} (${file_idx} of ${files.length}).`);
 
     // retry upload maxRetries times
     for (let loop_idx = 0; loop_idx < maxRetries; loop_idx++) {
-
-      // Send the auth request to get the upload URL
-      const auth_response = await upload_step_init(dt_upload_api_key);
-
-      let auth_json;
-      try {
-        auth_json = await auth_response.json();
-      } catch (err) {
-        core.setFailed(err);
-      }
-
-      if (auth_response.status !== 200) {
-        // handles auth failure
-        core.setFailed(auth_json);
-        break;
-      }
-
       // Send the scan request with file
       console.log("Starting upload...");
-      const response = await fetch(
-        auth_json.upload_url,
+
+      // Send the auth request to get the upload URL
+      const uploadUrl = await uploadStepInit(dt_upload_api_key);
+      console.log(`Using upload URL: ${uploadUrl}`);
+
+      const jsonData = uploadStepLast(
+        uploadUrl,
         {
-          "method": "POST",
-          "body": form,
-        },
+        }
       );
       console.log("Finished upload.");
 
-      let jsonformat;
-      try {
-        jsonformat = await response.json();
-      } catch (err) {
-        core.setFailed(err);
-      }
-      output.push(jsonformat)
-      console.log("Response: HTTP/" + response.status);
-      console.log(jsonformat);
-
-      // Check the response
-      // If we receive 409 (ownership conflict) or if this is the last try, bail out
-      if (response.status === 200) {
-        break;
-      } else if (response.status === 409) {
-        core.setFailed(jsonformat);
-        break;
-      } else if (loop_idx == (maxRetries - 1)) {
-        core.setFailed(jsonformat);
-      }
+      output.push(jsonData)
     }
   }
 
